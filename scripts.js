24d4421f4dc5d2fbc7ab86696827f164d8e8064d
Márcia Cristina da Silva Santos <marcia.cssantos95@gmail.com>
2021-09-07 17:42:11 -0300

    abrindo modal com js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
 const modalOverlay = document.querySelector('.modal-overlay')
 
 const cards = document.querySelectorAll('.card')
+
+for(let card of cards){
+    card.addEventListener("click", function(){
+        modalOverlay.classList.add('active')
+    })
+}
